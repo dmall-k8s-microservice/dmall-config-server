704a62af110ff270ebfdf123be92c8193d73c6d2
linwenjun <linwenjun@163.com>
2018-02-01 10:52:08 +0800

    change config

--- deployToDEV.sh
+++ deployToDEV.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
+cat config-server.yaml
+
 kubectl --kubeconfig=$KUBECTL_CONFIG_FILE delete -f config-server.yaml
 kubectl --kubeconfig=$KUBECTL_CONFIG_FILE create -f config-server.yaml
